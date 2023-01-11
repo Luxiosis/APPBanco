7258d4d5b0c2b90b1762b8b50e75b66b72b67b2e
Luxiosis <90099405+Luxiosis@users.noreply.github.com>
2023-01-11 10:06:45 -0300

    Update PessoaJuridica.java

--- PessoaJuridica.java
+++ PessoaJuridica.java
@@ -2,29 +2,59 @@ public class PessoaJuridica extends Conta {
     public PessoaJuridica(String nome, int numero, double saldo){
         super(nome, numero, saldo);
     }
-    
+
+
+    @Override
+    public void sacar(double valor){
+        if(getSaldo() >= valor){
+            double valor_c_taxa = valor - (valor * 0.005);
+            setSaldo(valor_c_taxa);
+            System.out.println("Sacado: " + valor_c_taxa + " (Houve 0,5% de taxa)");
+            System.out.println("Saldo atual: " + getSaldo() + "\n");
+        } else {
+            System.out.println("Saldo insuficiente. Faça um depósito\n");
+        }
+    }
+
+
+
+    @Override
+    public void transferir(double valor){
+        int trans_conta;
+        if(getSaldo() >= valor){
+            System.out.println("Digite o número da conta na qual você deseja transferir: ");
+            trans_conta = sc.nextInt();
+            double valor_c_taxa = valor - (valor * 0.005);
+            setSaldo(valor_c_taxa);
+            System.out.println("Foram transferidos " + valor_c_taxa + " para a conta " + trans_conta + " (Houve 0,5% de taxa)");
+            System.out.println("Saldo atual: " + getSaldo()+ "\n");
+        } else {
+            System.out.println("Saldo insuficiente. Faça um depósito\n");
+        }
+    }
+
     public void exibeContaJuridica(){
         System.out.println("\t Escolha o tipo de conta que você quer abrir: ");
         System.out.println("1 - Conta Corrente");
         System.out.println("2 - Conta Investimento");
-    } 
-    
+    }
+
     public void escolheContaJuridica(String tipo_conta){
         switch(tipo_conta){
-                case "1":
-                    System.out.print("Você escolheu conta corrente.\n");
-                    System.out.println("");
-                    break;
-                case "2":
-                    System.out.print("Você escolheu conta investimento.\n");
-                    System.out.println("");
-                    break;
-                default:
-                    System.out.println("Opção inválida");
-                    System.out.println("");
+            case "1":
+                System.out.print("Você escolheu conta corrente.\n");
+                System.out.println("");
+                break;
+            case "2":
+                System.out.print("Você escolheu conta investimento.\n");
+                System.out.println("");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                System.out.println("");
         }
     }
-    
+
     public void funcionalidades() {
         String funcao;
         do {
@@ -33,7 +63,7 @@ public class PessoaJuridica extends Conta {
             escolheFuncao(funcao);
         } while(!funcao.equals("5"));
     }
-    
+
     public void funcionalidadesInvestimento() {
         String funcao;
         do {
@@ -42,7 +72,7 @@ public class PessoaJuridica extends Conta {
             escolheFuncaoInvestimento(funcao);
         } while(!funcao.equals("5"));
     }
-    
+
     public void exibeMenu(){
         System.out.println("\t Escolha a opção desejada");
         System.out.println("1 - Sacar");
@@ -52,14 +82,14 @@ public class PessoaJuridica extends Conta {
         System.out.println("5 - Sair\n");
         System.out.print("Opção: ");
     }
-    
+
     public void escolheFuncao(String funcao){
         double valor;
         switch(funcao){
             case "1":
                 System.out.print("Quanto deseja sacar: ");
                 valor = sc.nextDouble();
-                sacarTaxa(valor);
+                sacar(valor);
                 break;
             case "2":
                 System.out.print("Quanto deseja depositar: ");
@@ -69,12 +99,12 @@ public class PessoaJuridica extends Conta {
             case "3":
                 System.out.print("Quanto deseja transferir: ");
                 valor = sc.nextDouble();
-                transferirTaxa(valor);
+                transferir(valor);
                 break;
             case "4":
                 extrato();
                 break;
-            case "5": 
+            case "5":
                 System.out.println("Sistema encerrado.");
                 break;
             default:
@@ -82,14 +112,12 @@ public class PessoaJuridica extends Conta {
                 System.out.println("");
         }
     }
-    
+
     public void escolheFuncaoInvestimento(String funcao){
         double valor;
         switch(funcao){
             case "1":
-                System.out.print("Quanto deseja investir: ");
-                valor = sc.nextDouble();
-                investirBonus(valor);
+                investir();
                 break;
             case "2":
                 System.out.print("Quanto deseja sacar: ");
@@ -104,7 +132,7 @@ public class PessoaJuridica extends Conta {
             case "4":
                 extratoInvestimento();
                 break;
-            case "5": 
+            case "5":
                 System.out.println("Sistema encerrado.");
                 break;
             default:
@@ -112,7 +140,7 @@ public class PessoaJuridica extends Conta {
                 System.out.println("");
         }
     }
-    
+
     public void exibeMenuInvestimento(){
         System.out.println("\t Escolha a opção desejada");
         System.out.println("1 - Investir");
